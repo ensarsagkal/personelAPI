c34cd1cddbd3f61092f3dd9d7c01f0797843ebcf
ensar <ensarsagkal@gmail.com>
2024-03-18 22:42:20 +0300

    dep route

--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -5,7 +5,9 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+const department=require("../controllers/department.controller")
 
-
+router.route("/").get(department.list).post(department.create)
+router.route("/:id").get(department.read).put(department.update).patch(department.update).delete(department.delete)
 /* ------------------------------------------------------- */
 module.exports = router
