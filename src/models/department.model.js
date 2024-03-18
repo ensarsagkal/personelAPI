71cee595b2c745fb5a912cd994b2def7cf27a3ae
ensar <ensarsagkal@gmail.com>
2024-03-18 21:46:55 +0300

    dep model

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,6 +1,19 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+const DepartmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  { collection: "departments", timestamps: true }
+);
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Department", DepartmentSchema);
