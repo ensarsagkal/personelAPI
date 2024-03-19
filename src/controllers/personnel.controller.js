610884bfdb9595bf3116b1e0558d49f790372ae4
ensar <ensarsagkal@gmail.com>
2024-03-19 15:35:08 +0300

    login logout

--- src/controllers/personnel.controller.js
+++ src/controllers/personnel.controller.js
@@ -1,82 +1,122 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 
-const Personnel = require("../models/personnel.model");
+const Personnel = require('../models/personnel.model')
 
 module.exports = {
-  list: async (req, res) => {
-    // const data = await Personnel.find(search).sort(sort).skip(skip).limit(limit)
-    const data = await res.getModelList(Personnel);
 
-    res.status(200).send({
-      error: false,
-      detail: await res.getModelListDetails(Personnel),
-      data, // data: data
-    });
-  },
+    list: async (req, res) => {
 
-  create: async (req, res) => {
-    const isLead = req.body || false;
-    if (isLead) {
-      await Personnel.updateMany(
-        { departmentId: req.body.departmentId, isLead: true },
-        { isLead: false }
-      );
-    }
-    const data = await Personnel.create(req.body);
+        const data = await res.getModelList(Personnel, {}, 'departmentId')
 
-    res.status(201).send({
-      error: false,
-      data,
-    });
-  },
+        res.status(200).send({
+            error: false,
+            detail: await res.getModelListDetails(Personnel),
+            data
+        })
 
-  read: async (req, res) => {
-    const data = await Personnel.findOne({ _id: req.params.id });
+    },
 
-    res.status(200).send({
-      error: false,
-      data,
-    });
-  },
+    create: async (req, res) => {
 
-  update: async (req, res) => {
-    const isLead = req.body?.isLead || false;
-    if (isLead) {
-      const { departmentId } = await Personnel.findOne(
-        { _id: req.params.id },
-        { departmentId: 1 }
-      );
-      await Personnel.updateMany(
-        { departmentId, isLead: true },
-        { isLead: false }
-      );
-    }
-    const data = await Personnel.updateOne({ _id: req.params.id }, req.body, {
-      runValidators: true,
-    });
+        // isLead Control:
+        const isLead = req.body?.isLead || false
+        if (isLead) {
+            const xyz = await Personnel.updateMany({ departmentId: req.body.departmentId, isLead: true }, { isLead: false })
+        }
 
-    res.status(202).send({
-      error: false,
-      data,
-      new: await Personnel.findOne({ _id: req.params.id }),
-    });
-  },
+        const data = await Personnel.create(req.body)
 
-  delete: async (req, res) => {
-    const data = await Personnel.deleteOne({ _id: req.params.id });
+        res.status(201).send({
+            error: false,
+            data
+        })
 
-    // const isDeleted = data.deletedCount >= 1 ? true : false;
-    // res.status(isDeleted ? 204 : 404).send({
-    //   error: !isDeleted,
-    //   data,
-    // });
+    },
 
-    res.status(data.deletedCount ? 204 : 404).send({
-      error: !data.deletedCount,
-      data,
-    });
-  },
-};
+    read: async (req, res) => {
+
+        const data = await Personnel.findOne({ _id: req.params.id })
+
+        res.status(200).send({
+            error: false,
+            data
+        })
+
+    },
+
+    update: async (req, res) => {
+
+        // isLead Control:
+        const isLead = req.body?.isLead || false
+        if (isLead) {
+            const { departmentId } = await Personnel.findOne({ _id: req.params.id }, { departmentId: 1 })
+            await Personnel.updateMany({ departmentId, isLead: true }, { isLead: false })
+        }
+
+        const data = await Personnel.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Personnel.findOne({ _id: req.params.id })
+        })
+    },
+
+    delete: async (req, res) => {
+
+        const data = await Personnel.deleteOne({ _id: req.params.id })
+
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        })
+    },
+
+    // LOGIN & LOGOUT
+
+    login: async (req, res) => {
+
+        const { username, password } = req.body
+
+        if (username && password) {
+
+            const user = await Personnel.findOne({ username, password })
+            if (user) {
+
+                // Set Session:
+                req.session = {
+                    id: user._id,
+                    password: user.password
+                }
+                // Set Cookie:
+                if (req.body?.rememberMe) {
+                    req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3 // 3 Days
+                }
+
+                res.status(200).send({
+                    error: false,
+                    user
+                })
+
+            } else {
+                res.errorStatusCode = 401
+                throw new Error('Wrong Username or Password.')
+            }
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Please entry username and password.')
+        }
+    },
+
+    logout: async (req, res) => {
+        // Set session to null:
+        req.session = null
+        res.status(200).send({
+            error: false,
+            message: 'Logout: Sessions Deleted.'
+        })
+    },
+}
--- src/routes/personnel.router.js
+++ src/routes/personnel.router.js
@@ -9,6 +9,10 @@ const personnel = require('../controllers/personnel.controller')
 
 // URL: /personnels
 
+// Login/logout:
+router.post('/login', personnel.login)
+router.all('/logout', personnel.logout)
+
 router.route('/')
     .get(personnel.list)
     .post(personnel.create)
