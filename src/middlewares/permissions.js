7bbba14dca36c6ac8b27f0e10e626d5ed9498fe2
ensar <ensarsagkal@gmail.com>
2024-03-21 21:45:15 +0300

    up

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -26,7 +26,7 @@ module.exports = {
         }
     },
 
-    isLead: (req, res, next) => {
+    isAdminOrLead: (req, res, next) => {
 
         const departmentId = req.params?.id
 
@@ -41,4 +41,24 @@ module.exports = {
             throw new Error('NoPermission: You must login and to be Admin or Department Lead.')
         }
     },
+
+    isAdminOrOwn: (req, res, next) => {
+
+        const personnelId = req.params?.id
+
+        if (
+            req.user
+            && req.user.isActive
+            && (
+                req.user.isAdmin
+                || req.user._id == personnelId
+            )
+        ) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and to be Admin or Record Owner.')
+        }
+
+    }
 }
