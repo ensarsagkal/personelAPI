3d54314bbc85ab6ac6a5c0ea43b9557dcb9f8db2
ensar <ensarsagkal@gmail.com>
2024-03-21 13:34:31 +0300

    fix

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -3,17 +3,20 @@
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 const router = require('express').Router()
+/* ------------------------------------------------------- *
+{
+    "username": "testF0",
+    "password": "1234"
+}
 /* ------------------------------------------------------- */
 
-const personnel = require('../controllers/personnel.controller')
+const auth = require('../controllers/auth.controller')
 
-// URL: /personnels
+// URL: /auth
 
 // Login/logout:
-router.post('/login', personnel.login)
-router.all('/logout', personnel.logout)
-
-
+router.post('/login', auth.login)
+router.all('/logout', auth.logout)
 
 /* ------------------------------------------------------- */
 module.exports = router
--- src/routes/personnel.router.js
+++ src/routes/personnel.router.js
@@ -9,10 +9,6 @@ const personnel = require('../controllers/personnel.controller')
 
 // URL: /personnels
 
-// // Login/logout:
-// router.post('/login', personnel.login)
-// router.all('/logout', personnel.logout)
-
 router.route('/')
     .get(personnel.list)
     .post(personnel.create)
